fab0db6404886d6bb8ed5a01c15c3143ef689118
Mario Schuetz <mayo2@gmx.de>
2020-06-03 14:12:05 +0200

    Refactor db functionality

--- mongodb.py
+++ mongodb.py
@@ -1,14 +1,29 @@
+import pymongo
 from pymongo import MongoClient
 
 MONGO_URI = 'mongodb://localhost:27017'
-client = MongoClient(MONGO_URI)
-db = client.phonebookdiver
+try:
+  client = MongoClient(MONGO_URI)
+  db = client.phonebookdiver 
+except pymongo.errors as err:
+  print(err)
+else:
+  print('CONNECTED to ', db.name)
 
-def create_collection(name):
-  collection = db[name]
+def get_database():
+  return db
+
+def get_collection(name):
+    return db[name]
 
 def insert_entry(entry, collection):
-  collection.insert_one(entry)
+  c = get_collection(collection)
+  c.insert_one(entry)
 
 def insert_entries(entries, collection):
-  collection.insert_many(entries)
+  c = get_collection(collection)
+  c.insert_many(entries)
+
+def upsert_entry(id, entry, collection):
+  c = get_collection(collection)
+  c.update(id, entry, upsert=True)
