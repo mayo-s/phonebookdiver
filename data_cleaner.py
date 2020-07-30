cf8ee4ad09856142fd2667127e34fcc63049f384
Mario Schuetz <mayo2@gmx.de>
2020-07-30 20:18:28 +0200

    Clean data from special chars to german umlaut

---
+++ data_cleaner.py
@@ -0,0 +1,68 @@
+from helper import get_main_dir, get_directories, get_files_in_dir, get_encoding, log
+
+# author: Mario Schuetz
+#
+# Clean data from special chars etc.
+#
+
+log('LB', '')
+log('INFO', 'DATA CLEANER Running')
+directories = []
+special_chars = {
+  '─': 'Ä',
+  'Σ': 'ä',
+  '╓': 'Ö',
+  '÷': 'ö',
+  '▄': 'Ü',
+  'ⁿ': 'ü',
+  '▀': 'ß',
+  '  ': ' ', # double space to single space
+  ' ': ' ', # tab to single space
+}
+
+def get_files():
+  directories = get_directories()
+  for dir in directories:
+    dirname = dir['name']
+    dir['files'] = get_files_in_dir(dirname)
+
+  return directories
+
+def clean_spec_chars(dir):
+  phonebookDir = get_main_dir()
+  dirname = dir['name']
+  files = dir['files']
+  enc = 'utf-8'
+  for file in files:
+
+    # only use utf-8 converted files
+    if('utf-8' not in file):
+      continue
+
+    info = f'Cleaning {dirname} {file}'
+    log('INFO', info)
+
+    with open(phonebookDir + dirname + file, 'r', encoding=enc) as data:
+      content = data.read()
+
+    for line in content:
+      line = check_substring(line)
+
+    with open(phonebookDir + dirname + file, 'w', encoding=enc) as data:
+      data = data.write(content)
+
+def check_substring(line):
+  for sc in special_chars:
+    while sc in line: 
+    	line = line.replace(sc, special_chars.get(sc))
+
+  return line
+
+
+# START
+directories = get_files()
+for dir in directories:
+  clean_spec_chars(dir)
+
+log('INFO', 'DATA CLEANER Done')
+
