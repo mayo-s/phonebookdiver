0004dfce1a046ad2ffde6af37d07a2cdab2785b9
Mario Schuetz <mayo2@gmx.de>
2020-08-06 12:58:31 +0200

    Clean up

--- src/components/heatmap/Filterbar.js
+++ src/components/heatmap/Filterbar.js
@@ -1,19 +1,15 @@
 import React from 'react';
 
 const Filterbar = () => {
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    // var instances = M.Dropdown.init(elems, options);
-  });
   return (
     <div className="wrapper grey darken-2">
       <div className="container">
         Slider here!
-        <a class='dropdown-trigger btn' href='#' data-target='field'>Choose</a>
-        <ul id='field' class='dropdown-content'>
+        <a className='dropdown-trigger btn' href='#' data-target='field'>Choose</a>
+        <ul id='field' className='dropdown-content'>
             <li><a href="#!">Firstname</a></li>
             <li><a href="#!">Lastname</a></li>
-            <li class="divider" tabindex="-1"></li>
+            <li className="divider" tabIndex="-1"></li>
             <li><a href="#!">City</a></li>
             <li><a href="#!">Zip</a></li>
         </ul>
