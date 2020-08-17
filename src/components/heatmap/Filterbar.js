5736a0c137e95be442c692d5e3dc4a4a6dc81b3c
Mario Schuetz <mayo2@gmx.de>
2020-08-17 20:02:05 +0200

    Get data from api

--- src/components/heatmap/Filterbar.js
+++ src/components/heatmap/Filterbar.js
@@ -5,7 +5,7 @@ class Filterbar extends Component {
   state = {
     collection: '',
     field: '',
-    search_string: '',
+    search_str: '',
   }
 
   handleChange = (e) => {
@@ -14,24 +14,43 @@ class Filterbar extends Component {
     })
   }
 
-  render () {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let url = 'http://localhost:5000/search?collection=' + this.state.collection + '&key=' + this.state.field + '&value=' + this.state.search_str;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.props.get_coords(data));
+  }
+
+  render() {
     return (
       <div className="wrapper grey darken-2">
-          <div className="row">
-            <div className="col s12 m4">
-              Slider here!
-            </div>
-            <div className="col s6 m4">
-
-            </div>
-            <div className="col s6 m4">
+        <div className="row">
+          <form onSubmit={this.handleSubmit}>
+            <div className="col s6 m3">
               <div className="input-field">
-                <label htmlFor="title">Search String</label>
-                <input className="white-text" type="text" id="search_string" onChange={this.handleChange} />
+                <label htmlFor="collection">Collection</label>
+                <input className="white-text" type="text" id="collection" onChange={this.handleChange} />
               </div>
             </div>
-          </div>
+            <div className="col s6 m3">
+              <div className="input-field">
+                <label htmlFor="field">Field</label>
+                <input className="white-text" type="text" id="field" onChange={this.handleChange} />
+              </div>
+            </div>
+            <div className="col s6 m3">
+              <div className="input-field">
+                <label htmlFor="search_str">Search string</label>
+                <input className="white-text" type="text" id="search_str" onChange={this.handleChange} />
+              </div>
+            </div>
+            <div className="col s6 m3">
+              <button className="btn center-align" >Search</button>
+            </div>
+          </form>
         </div>
+      </div>
     )
   }
 
--- src/components/heatmap/Map.js
+++ src/components/heatmap/Map.js
@@ -3,10 +3,29 @@ import Filterbar from './Filterbar';
 import Heatmap from './Heatmap';
 
 class Map extends Component {
+
+  state = {
+    collection: '',
+    field: '',
+    search_string: '',
+    coords: []
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  get_coords = (response) => {
+    this.setState({coords: response});
+    // console.log(this.state.coords);
+  }
+
   render() {
     return (
       <div className="dashboard">
-        <Filterbar />
+        <Filterbar get_coords={this.get_coords}/>
         <Heatmap />
       </div>
     )
