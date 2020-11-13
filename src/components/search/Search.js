f51a41a27445f24bf182ff4ffd1452ce3efe319b
Mario Schuetz <mayo2@gmx.de>
2020-11-13 14:34:28 +0100

    Change table style

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -21,9 +21,9 @@ class Search extends Component {
     e.preventDefault();
     const isValid = this.validateForm();
 
-    if(isValid) {
+    if (isValid) {
       let queryMsg = 'Querying the years from ' + this.state.collection_start.substr(0, 4) + ' to ' + this.state.collection_end.substr(0, 4) + ' for ' + this.state.key + ' = ' + this.state.search_str + '.';
-      this.setState({queryMsg})
+      this.setState({ queryMsg })
       let url = 'http://localhost:5000/search?start=' + this.state.collection_start + '&end=' + this.state.collection_end + '&key=' + this.state.key + '&value=' + this.state.search_str;
       fetch(url)
         .then(response => response.json())
@@ -34,39 +34,37 @@ class Search extends Component {
   validateForm = () => {
     let keyError, strError, startError = '';
 
-    if (!this.state.collection_start) this.setState({collection_start: this.state.first});
-    if (!this.state.collection_end) this.setState({collection_end: this.state.last});
     if (parseInt(this.state.collection_start.substr(0, 4)) > parseInt(this.state.collection_end.substr(0, 4))) {
       startError = 'First YEAR value must be lower than second.';
     }
 
-    if(!this.state.key) {
+    if (!this.state.key) {
       keyError = 'Please choose a FIELD to query';
     }
-    if(!this.state.search_str || this.state.search_str.length < 2) {
+    if (!this.state.search_str || this.state.search_str.length < 2) {
       strError = 'Search string cannot be empty and must have at least 2 letters';
     }
 
-    if(startError || keyError || strError) {
-      this.setState({startError, keyError, strError});
+    if (startError || keyError || strError) {
+      this.setState({ startError, keyError, strError });
       return false;
     }
 
-    this.setState({startError, keyError, strError});
+    this.setState({ startError, keyError, strError });
     return true
   }
 
   update_resultView = (data) => {
     console.log(data);
     let queryMsg = this.state.queryMsg + ' Found ' + data.length + ' entrie(s).';
-    this.setState({queryMsg, results: data});
+    this.setState({ queryMsg, results: data });
   }
 
   handleChange = (e) => {
-      this.setState({
-        [e.target.id]: e.target.value
-      });
-      // console.log(this.state);
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+    // console.log(this.state);
   }
 
   getCollections = () => {
@@ -78,9 +76,10 @@ class Search extends Component {
         let last = '0000_Q0';
         for (let d in data) {
           options.push(<option value={d} >{d}</option>);
-          if(parseInt(first.substr(0, 4)) > parseInt(d.substr(0, 4))) first = d;
-          if(parseInt(last.substr(0, 4)) < parseInt(d.substr(0, 4))) last = d;
+          if (parseInt(first.substr(0, 4)) > parseInt(d.substr(0, 4))) first = d;
+          if (parseInt(last.substr(0, 4)) < parseInt(d.substr(0, 4))) last = d;
         }
+        // by default range over all available collections
         this.setState({ cOptions: options, collection_start: first, collection_end: last });
       })
   }
@@ -89,15 +88,52 @@ class Search extends Component {
 
     return (
       <tr>
-        <td>{result.firstname}</td>
         <td>{result.lastname}</td>
-        <td>{result.zip}</td>
+        <td>{result.firstname}</td>
         <td>{result.city}</td>
+        <td>{result.zip}</td>
         <td>{result.street} {result.street_number}</td>
         <td>{result.area_code}</td>
         <td>{result.phonenumber}</td>
-        <td>{result.appearance}</td>
+        <td>{this.beautify_appearance_data(result.appearance)}</td>
       </tr>)
+  }
+
+  beautify_appearance_data = (list) => {
+    let data = '';
+    let first = true;
+    for (let i in list) {
+      if(!first) data += ', ' + list[i];
+      else {
+        data += list[i];
+        first = false;
+      }
+    }
+    return data
+  }
+
+  createTable = () => {
+    return (
+      <table className="striped highlight ">
+        <thead>
+          <tr>
+            <th>Lastname</th>
+            <th>Firstname</th>
+            <th>City</th>
+            <th>ZIP</th>
+            <th>Street</th>
+            <th>Area Code</th>
+            <th>Phone Number</th>
+            <th>Appearance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.results.map((result, index) => {
+            return this.addTableRow(result)
+          })}
+        </tbody>
+      </table>
+    )
   }
 
   render() {
@@ -110,50 +146,50 @@ class Search extends Component {
         <div className="wrapper grey darken-2">
           <form onSubmit={this.handleSubmit}>
 
-              <div className="row">
-                <div className="input-field col s6 m3">
-                  <select className="browser-default" id="collection_start" onChange={this.handleChange} >
-                    <option value="" disabled selected>Choose Start YEAR to query</option>
-                    {this.state.cOptions}
-                  </select>
-                  {this.state.startError ? (
-                    <div style={{fontSize: 12, color: "red"}}>{this.state.startError}</div>
-                  ) : null}
-                </div>
-
-                <div className="input-field col s6 m3">
-                  <select className="browser-default" id="collection_end" onChange={this.handleChange} >
-                    <option value="" disabled selected>Choose End YEAR to query</option>
-                    {this.state.cOptions}
-                  </select>
-                </div>
-                <div className="input-field col s6 m3">
-                  <button className="btn center-align" >Search</button>
-                </div>
+            <div className="row">
+              <div className="input-field col s6 m3">
+                <select className="browser-default" id="collection_start" onChange={this.handleChange} >
+                  <option value="" disabled selected>Choose Start YEAR to query</option>
+                  {this.state.cOptions}
+                </select>
+                {this.state.startError ? (
+                  <div style={{ fontSize: 12, color: "red" }}>{this.state.startError}</div>
+                ) : null}
               </div>
 
-              <div className="row">
-                <div className="input-field col s6 m3">
-                  <select className="browser-default" id="key" onChange={this.handleChange} >
-                    <option value="" disabled selected>Choose FIELD to query</option>
-                    <option value="lastname">Lastname</option>
-                    <option value="firstname">Firstname</option>
-                    {/* <option value="street">Street</option> */}
-                  </select>
-                  {this.state.keyError ? (
-                    <div style={{fontSize: 12, color: "red"}}>{this.state.keyError}</div>
+              <div className="input-field col s6 m3">
+                <select className="browser-default" id="collection_end" onChange={this.handleChange} >
+                  <option value="" disabled selected>Choose End YEAR to query</option>
+                  {this.state.cOptions}
+                </select>
+              </div>
+              <div className="input-field col s6 m3">
+                <button className="btn center-align" >Search</button>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="input-field col s6 m3">
+                <select className="browser-default" id="key" onChange={this.handleChange} >
+                  <option value="" disabled selected>Choose FIELD to query</option>
+                  <option value="lastname">Lastname</option>
+                  <option value="firstname">Firstname</option>
+                  {/* <option value="street">Street</option> */}
+                </select>
+                {this.state.keyError ? (
+                  <div style={{ fontSize: 12, color: "red" }}>{this.state.keyError}</div>
+                ) : null}
+              </div>
+              <div className="col s6 m3">
+                <div className="input-field">
+                  <label htmlFor="search_str">Search string</label>
+                  <input className="white-text" type="text" id="search_str" onChange={this.handleChange} />
+                  {this.state.strError ? (
+                    <div style={{ fontSize: 12, color: "red" }}>{this.state.strError}</div>
                   ) : null}
                 </div>
-                <div className="col s6 m3">
-                  <div className="input-field">
-                    <label htmlFor="search_str">Search string</label>
-                    <input className="white-text" type="text" id="search_str" onChange={this.handleChange} />
-                    {this.state.strError ? (
-                      <div style={{fontSize: 12, color: "red"}}>{this.state.strError}</div>
-                    ) : null}
-                  </div>
-                </div>
               </div>
+            </div>
 
             <div className="row">
               {this.state.queryMsg ? (
@@ -163,28 +199,12 @@ class Search extends Component {
           </form>
         </div>
 
-        <div className="row">
-          <table className="striped highlight">
-            <thead>
-              <tr>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>ZIP</th>
-                <th>City</th>
-                <th>Street</th>
-                <th>Area Code</th>
-                <th>Phone Number</th>
-                <th>Appearance</th>
-              </tr>
-            </thead>
-            <tbody>
-              {results.map((result, index) => {
-                return this.addTableRow(result)
-              })}
-            </tbody>
-          </table>
+        <div className="row querytable">
+          <div className="card material-table">
+            {this.createTable()}
+          </div>
         </div>
-        
+
       </div>
     )
   }
