ce48e42b01d6e4e685683c428d2e9396af98d3ec
Mario Schuetz <mayo2@gmx.de>
2020-11-18 13:06:28 +0100

    Create indexes after upload

--- tools/main.py
+++ tools/main.py
@@ -1,10 +1,12 @@
 from encoding_converter import encoder
 from reader import reader
 from uploader import uploader
+from db_indexer import create_indexes
 
 def start():
-    encoder()
+    # encoder()
     reader()
     uploader()
+    create_indexes()
 
 start()
