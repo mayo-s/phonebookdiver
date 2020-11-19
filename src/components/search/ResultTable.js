51e48948880f39cdf4fdd6de12733fa62641fe4e
Mario Schuetz <mayo2@gmx.de>
2020-11-19 10:17:41 +0100

    Clean up sorting function

--- src/components/search/ResultTable.js
+++ src/components/search/ResultTable.js
@@ -16,12 +16,12 @@ class ResultTable extends Component {
         <td>{this.beautify_appearance_data(result.appearance)}</td>
       </tr>)
   }
-  
+
   beautify_appearance_data = (list) => {
     let data = '';
     let first = true;
     for (let i in list) {
-      if(!first) data += ', ' + list[i];
+      if (!first) data += ', ' + list[i];
       else {
         data += list[i];
         first = false;
@@ -29,7 +29,7 @@ class ResultTable extends Component {
     }
     return data
   }
-  
+
   createTable = (results) => {
 
     return (
@@ -55,29 +55,26 @@ class ResultTable extends Component {
     )
   }
 
+  // TODO - REFACTOR sorting algorithm
   sortByField = (field) => {
     let sorted_results = this.props.results;
-    if(field === 'lastname') sorted_results = this.props.results.sort((r1, r2) => (r1.lastname > r2.lastname ? 1 : -1));
-    else if(field === 'firstname') sorted_results = this.props.results.sort((r1, r2) => (r1.firstname > r2.firstname ? 1 : -1));
-    else if (field === 'city') sorted_results = this.props.results.sort((r1, r2) => (r1.city > r2.city ? 1 : -1));
-    else if(field === 'zip') sorted_results = this.props.results.sort((r1, r2) => (r1.zip > r2.zip ? 1 : -1));
-    else if(field === 'area_code') sorted_results = this.props.results.sort((r1, r2) => (r1.area_code > r2.area_code ? 1 : -1));
+    sorted_results = this.props.results.sort((r1, r2) => (r1[field] > r2[field] ? 1 : -1));
 
     this.props.update_results(sorted_results);
   }
 
   render() {
-    
-    return(
+
+    return (
       <div>
         {this.props.results.length ? (
           <div className="card">
-          {this.createTable(this.props.results)}
-        </div>
-        ): null }
+            {this.createTable(this.props.results)}
+          </div>
+        ) : null}
       </div>
     )
   }
 }
-  
+
 export default ResultTable
