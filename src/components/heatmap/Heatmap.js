6c29c45895dff06468974b2d09c17753a2d04f74
Mario Schuetz <mayo2@gmx.de>
2021-02-26 21:55:07 +0100

    Normalize intensity count and adjust gradient

--- src/components/heatmap/Heatmap.js
+++ src/components/heatmap/Heatmap.js
@@ -21,14 +21,17 @@ class Heatmap extends Component {
   render() {
     const position = [this.state.center.lat, this.state.center.lng];
     const points = this.props.heatMapData.positions;
+    console.log(points)
     const zoom = this.state.zoom;
     const radius = this.state.radius;
     const blur = this.state.blur;
     const max = this.state.max;
-    const gradient = {
-      0.1: '#89BDE0', 0.2: '#96E3E6', 0.4: '#82CEB6',
-      0.6: '#FAF3A5', 0.8: '#F5D98B', 1.0: '#DE9A96'
-    };
+    const gradient = // { 0.0: 'green', 0.5: 'yellow', 1.0: 'red'}
+    { 0.0: 'green', 0.2: '#FFFF33', 0.4: '#FFB266', 0.8: '#FF8000', 1.0: 'red' }
+    // {
+    //   0.1: '#89BDE0', 0.2: '#96E3E6', 0.4: '#82CEB6',
+    //   0.6: '#FAF3A5', 0.8: '#F5D98B', 1.0: '#DE9A96'
+    // };
 
     return (
 
@@ -40,7 +43,7 @@ class Heatmap extends Component {
           points={points}
           longitudeExtractor={m => m[1]}
           latitudeExtractor={m => m[0]}
-          intensityExtractor={m => parseFloat(m[2])} 
+          intensityExtractor={m => parseFloat(m[2] * 10)} 
           radius={radius} 
           blur={blur}
           max={max}
